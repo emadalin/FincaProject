3a4b2fd25a7a4583389c1bb82ec540e67a8a481d
Ella Madalinski <ella.madalinski24@d155.org>
2026-02-19 20:41:26 -0600

    Changed recommendations screen to buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,45 +1,169 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { Image } from 'expo-image';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
+  const handleRestaurant = () => {
+    Alert.alert('Button Pressed!');
+  };
+  const handleCafe = () => {
+    Alert.alert('Button Pressed!');
+  };
+  const handleDayTrips = () => {
+    Alert.alert('Button Pressed!');
+  };
+  const handleOvernightTrips = () => {
+    Alert.alert('Button Pressed!');
+  };
+  const handleTransportation = () => {
+    Alert.alert('Button Pressed!');
+  };
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/OutsideFincaPhoto.jpg')}
           style={styles.reactLogo}
         />
       }>
+
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello World!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Recommendations</ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+
+      <View style={styles.container}>
+        <Pressable
+          style={styles.boxButton}
+          onPress={handleRestaurant}>
+            <Text style={styles.buttonText}>Restaurants and Sodas</Text>
+        </Pressable>
+      </View>
+      <View style={styles.container}>
+        <Pressable
+          style={styles.boxButton}
+          onPress={handleCafe}>
+            <Text style={styles.buttonText}>Cafés and Study Spots</Text>
+        </Pressable>
+      </View>
+      <View style={styles.container}>
+        <Pressable
+          style={styles.boxButton}
+          onPress={handleDayTrips}>
+            <Text style={styles.buttonText}>Day Trips</Text>
+        </Pressable>
+      </View>
+      <View style={styles.container}>
+        <Pressable
+          style={styles.boxButton}
+          onPress={handleOvernightTrips}>
+            <Text style={styles.buttonText}>Overnight Trips</Text>
+        </Pressable>
+      </View>
+      <View style={styles.container}>
+        <Pressable
+          style={styles.boxButton}
+          onPress={handleTransportation}>
+            <Text style={styles.buttonText}>Transportation</Text>
+        </Pressable>
+      </View>
+
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    width: '100%',
+    height: 250,
+    position: 'absolute',
+  },
+  container: {
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+  boxButton: {
+    backgroundColor: '#9CAF88',
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginVertical: 0,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 17,
+    fontWeight: 'bold'
+  },
+});
+
+      {/* <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Restaurants and Sodas</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
+          <ThemedText type="defaultSemiBold">La Cabriola - Ciudad Colon</ThemedText>
+          {'\n'}description of La Cabriola
+          <ThemedText type="defaultSemiBold">{'\n'}Nuestra Tierra - San Jose</ThemedText>
+          {'\n'}description of Nuestra Tierrra
+          {' '}
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Cafés and Study Spots</ThemedText>
+        <ThemedText>
+          <ThemedText type="defaultSemiBold">Casa Amara - Ciudad Colon</ThemedText>
+          {'\n'}description of Casa Amara
+          <ThemedText type="defaultSemiBold">{'\n'}Tulsi Café - Ciudad Colon</ThemedText>
+          {'\n'}description of Tulsi
+          {' '}
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Day Trips</ThemedText>
+        <ThemedText>
+          <ThemedText type="defaultSemiBold">Baldi Hot Springs</ThemedText>
+          {'\n'}description of Baldi
+          {' '}
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Overnight Trips</ThemedText>
+        <ThemedText>
+          <ThemedText type="defaultSemiBold">Idea</ThemedText>
+          {'\n'}description of Idea
+          {' '}
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Transportation</ThemedText>
+        <ThemedText>
+          <ThemedText type="defaultSemiBold">Uber</ThemedText>
+          {'\n'}description of Uber
+          <ThemedText type="defaultSemiBold">{'\n'}Bus</ThemedText>
+          {'\n'}description of Bus
+          <ThemedText type="defaultSemiBold">{'\n'}Interbus</ThemedText>
+          {'\n'}description of Interbus
+          <ThemedText type="defaultSemiBold">{'\n'}Gata&friends</ThemedText>
+          {'\n'}description of Gata
+          {' '}
+        </ThemedText>
+      </ThemedView>
+        <ThemedText></ThemedText>
+      <ThemedView style={styles.stepContainer}>
         <Link href="/modal">
           <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+            <ThemedText type="subtitle">Step 2: Resources</ThemedText>
           </Link.Trigger>
           <Link.Preview />
           <Link.Menu>
@@ -73,26 +197,4 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+      </ThemedView> */}
